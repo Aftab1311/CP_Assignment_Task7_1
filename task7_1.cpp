bf01b4cfd1d814b664704fba7de6c434a35881a1
Aftab Ahmed <88955526+Aftab1311@users.noreply.github.com>
2021-08-23 19:35:41 +0530

    Add files via upload

---
+++ task7_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int Power(int b, int p)
+{
+    if (p == 0)
+        return 1;
+    else
+        return (b * Power(b, p - 1));
+}
+int main()
+{
+    int b , p ;
+    cout<<"Enter base : ";
+    cin>>b;
+    cout<<"Enter power : ";
+    cin>>p;
+    cout << "The answer of " << b << "^" << p << " is : " << Power( b,  p);
+
+    return 0;
+}
